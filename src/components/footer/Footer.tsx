8767e5c6f27ff528e5dc504f336ec3f32edaff10
barez azad <barez.001159544@gmail.com>
2026-04-26 19:15:07 +0300

    Footer updated

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="flex w-full flex-col items-center justify-between px-1 pb-8 pt-3 lg:px-8 xl:flex-row">
       <h5 className="mb-4 text-center text-sm font-medium text-gray-600 sm:!mb-0 md:text-lg">
         <p className="mb-4 text-center text-sm text-gray-600 sm:!mb-0 md:text-base">
-          ©{1900 + new Date().getFullYear()} Horizon UI. All Rights Reserved.
+          ©{new Date().getFullYear()} The Edge. All Rights Reserved.
         </p>
       </h5>
       <div>
@@ -11,37 +11,10 @@ const Footer = () => {
           <li>
             <a
               target="blank"
-              href="mailto:hello@simmmple.com"
+              href="https://youtube.com/@theedgehq?si=YTV6oO45xL6Uhj4U"
               className="text-base font-medium text-gray-600 hover:text-gray-600"
             >
-              Support
-            </a>
-          </li>
-          <li>
-            <a
-              target="blank"
-              href="https://simmmple.com/licenses"
-              className="text-base font-medium text-gray-600 hover:text-gray-600"
-            >
-              License
-            </a>
-          </li>
-          <li>
-            <a
-              target="blank"
-              href="https://simmmple.com/terms-of-service"
-              className="text-base font-medium text-gray-600 hover:text-gray-600"
-            >
-              Terms of Use
-            </a>
-          </li>
-          <li>
-            <a
-              target="blank"
-              href="https://blog.horizon-ui.com/"
-              className="text-base font-medium text-gray-600 hover:text-gray-600"
-            >
-              Blog
+              YouTube
             </a>
           </li>
         </ul>
