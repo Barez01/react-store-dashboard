aa153c34dcc7d13eb6b4fe17b7e011ee6a6c8361
barez azad <barez.001159544@gmail.com>
2026-04-29 02:46:57 +0300

    the profile nav bar dark mode is working permanently

--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -16,7 +16,8 @@ import {
   MdGroup,
   MdGroups,
 } from "react-icons/md";
-
+import avatar from "assets/img/avatars/avatar4.png";
+import React from "react";
 import Widget from "components/widget/Widget";
 import CheckTable from "views/admin/default/components/CheckTable";
 import ComplexTable from "views/admin/default/components/ComplexTable";
@@ -30,6 +31,7 @@ import { FiSearch, FiAlignJustify } from "react-icons/fi";
 import { RiSunFill, RiMoonFill } from "react-icons/ri";
 
 const Dashboard = () => {
+  const [darkmode, setDarkmode] = React.useState(false);
   return (
     <div>
       {/* Card widget */}
@@ -53,7 +55,7 @@ const Dashboard = () => {
           subtitle={"$10"}
           path="nft-marketplace"
         />
-        <div className="col-span-1 md:col-span-2 lg:col-span-3 3xl:col-span-3 w-full relative flex h-[92px] flex-grow items-center justify-around gap-2 rounded-[20px] border-[1px] border-gray-200 bg-white bg-clip-border px-2 py-2 shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none md:flex-grow-0 md:gap-1 xl:gap-2">
+        <div className="relative col-span-1 flex h-[92px] w-full flex-grow items-center justify-around gap-2 rounded-[20px] border-[1px] border-gray-200 bg-white bg-clip-border px-2 py-2 shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none md:col-span-2 md:flex-grow-0 md:gap-1 lg:col-span-3 xl:gap-2 3xl:col-span-3">
           <div className="flex h-full items-center rounded-[15px] bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[225px]">
             <p className="pl-3 pr-2 text-xl">
               <FiSearch className="h-4 w-4 text-gray-400 dark:text-white" />
@@ -165,17 +167,17 @@ const Dashboard = () => {
           />
           <div
             className="cursor-pointer text-gray-600"
-            // onClick={() => {
-            //   if (darkmode) {
-            //     document.body.classList.remove("dark");
-            //     setDarkmode(false);
-            //   } else {
-            //     document.body.classList.add("dark");
-            //     setDarkmode(true);
-            //   }
-            // }}
+            onClick={() => {
+              if (darkmode) {
+                document.body.classList.remove("dark");
+                setDarkmode(false);
+              } else {
+                document.body.classList.add("dark");
+                setDarkmode(true);
+              }
+            }}
           >
-            {false ? (
+            {darkmode ? (
               <RiSunFill className="h-4 w-4 text-gray-600 dark:text-white" />
             ) : (
               <RiMoonFill className="h-4 w-4 text-gray-600 dark:text-white" />
@@ -186,7 +188,7 @@ const Dashboard = () => {
             button={
               <img
                 className="h-10 w-10 rounded-full"
-                // src={avatar}
+                src={avatar}
                 alt="Elon Musk"
               />
             }
