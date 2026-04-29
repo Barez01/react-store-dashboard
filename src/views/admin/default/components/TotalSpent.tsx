8a57806ae0bff718d3248168f0536c9e22047efe
barez azad <barez.001159544@gmail.com>
2026-04-30 01:16:48 +0300

    monthly profit chart updated

--- src/views/admin/default/components/TotalSpent.tsx
+++ src/views/admin/default/components/TotalSpent.tsx
@@ -16,15 +16,15 @@ const TotalSpent = () => {
       <div className="flex justify-between">
         <button className="linear mt-1 flex items-center justify-center gap-2 rounded-lg bg-lightPrimary p-2 text-gray-600 transition duration-200 hover:cursor-pointer hover:bg-gray-100 active:bg-gray-200 dark:bg-navy-700 dark:hover:opacity-90 dark:active:opacity-80">
           <MdOutlineCalendarToday />
-          <span className="text-sm font-medium text-gray-600">This month</span>
+          <span className="text-sm font-medium text-gray-600">Monthly Profit</span>
         </button>
-        <button className="!linear z-[1] flex items-center justify-center rounded-lg bg-lightPrimary p-2 text-brand-500 !transition !duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-navy-700 dark:text-white dark:hover:bg-white/20 dark:active:bg-white/10">
+        {/* <button className="!linear z-[1] flex items-center justify-center rounded-lg bg-lightPrimary p-2 text-brand-500 !transition !duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-navy-700 dark:text-white dark:hover:bg-white/20 dark:active:bg-white/10">
           <MdBarChart className="h-6 w-6" />
-        </button>
+        </button> */}
       </div>
 
       <div className="flex h-full w-full flex-row justify-between sm:flex-wrap lg:flex-nowrap 2xl:overflow-hidden">
-        <div className="flex flex-col">
+        {/* <div className="flex flex-col">
           <p className="mt-[20px] text-3xl font-bold text-navy-700 dark:text-white">
             $37.5K
           </p>
@@ -35,7 +35,7 @@ const TotalSpent = () => {
               <p className="text-sm font-bold text-green-500"> +2.45% </p>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="h-full w-full">
           <LineChart
             chartOptions={lineChartOptionsTotalSpent}
